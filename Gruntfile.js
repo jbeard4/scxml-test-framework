481e11d0dea66ef9026cb25420f691d3717da56c
Jacob Beard <jake@minnow.io>
2016-08-13 01:58:22 -0400

    Added Gruntfile to facilitate release.

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
+  var browsers = require('./grunt/browsers');
+
+  grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      release: {
+        options: {
+          github: {
+            repo: 'jbeard4/scxml-test-framework', //put your user/repo here
+            accessTokenVar: 'GITHUB_ACCESS_TOKEN', //ENVIRONMENT VARIABLE that contains GitHub Access Token
+          }
+        }
+      }
+  });
+};
